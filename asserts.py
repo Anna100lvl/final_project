66b5e6b58a190ffccd47393a302730e9b4176b16
Anna100lvl <115748396+Anna100lvl@users.noreply.github.com>
2023-01-16 23:47:13 +0500

    .py file with asserts for task №0

--- asserts.py
+++ asserts.py
@@ -1,3 +1,13 @@
+import pandas as pd
+
+
+def create_data_frame(file_name: str) -> pd.core.frame.DataFrame:
+    """Эта функция принимает в качестве аргумента название .csv файла и создает из него датафрейм."""
+    original_df = pd.read_csv(file_name)
+    nonduplicate_recipes = original_df[original_df.index < 31]
+    return nonduplicate_recipes
+
+
 def rework_time_to_minutes(time_string: str) -> int:
     """Функция, которая преобразовывает строку формата "% hrs % mins" в целочисленное значение минут, вычисленное для этой строки"""
     time_string = str(time_string)
@@ -10,14 +20,3 @@ def rework_time_to_minutes(time_string: str) -> int:
     elif 'mins' in time_string:
         total_minutes = int(time_list[0])
     return total_minutes
-
-
-assert rework_time_to_minutes('random word') == 0
-assert rework_time_to_minutes('random word else shit') == 0
-assert rework_time_to_minutes(' ') == 0
-assert rework_time_to_minutes(nonduplicate_recipes.total_time[1]) == 90
-assert rework_time_to_minutes(nonduplicate_recipes.total_time[0]) == 0
-assert rework_time_to_minutes('0 hrs 0 mins') == 0
-assert rework_time_to_minutes('7 hrs 15 mins') == 435
-assert rework_time_to_minutes('7 hrs') == 420
-assert rework_time_to_minutes('15 mins') == 15
