9e0b270032b52490a6c06fc2f58fe08ff810c874
Anna100lvl <115748396+Anna100lvl@users.noreply.github.com>
2023-01-18 19:47:03 +0500

    returned asserts

--- asserts.py
+++ asserts.py
@@ -20,3 +20,14 @@ def rework_time_to_minutes(time_string: str) -> int:
     elif 'mins' in time_string:
         total_minutes = int(time_list[0])
     return total_minutes
+
+
+assert rework_time_to_minutes('random word') == 0
+assert rework_time_to_minutes('random word else shit') == 0
+assert rework_time_to_minutes(' ') == 0
+assert rework_time_to_minutes(nonduplicate_recipes.total_time[1]) == 90
+assert rework_time_to_minutes(nonduplicate_recipes.total_time[0]) == 0
+assert rework_time_to_minutes('0 hrs 0 mins') == 0
+assert rework_time_to_minutes('7 hrs 15 mins') == 435
+assert rework_time_to_minutes('7 hrs') == 420
+assert rework_time_to_minutes('15 mins') == 15
